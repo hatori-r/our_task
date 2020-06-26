f5746cc1c724229460862aa9eab8e3b806663c1a
hatori-r <aruue368@gmail.com>
2020-06-26 09:50:59 +0900

    commit

--- app/controllers/tasks/searches_controller.rb
+++ app/controllers/tasks/searches_controller.rb
@@ -1,5 +1,5 @@
 class Tasks::SearchesController < ApplicationController
   def index
-    @tasks = Task.search(params[:keyword])
+    @tasks = Task.search(params[:keyword]).order("created_at DESC").page(params[:page]).per(10)
   end
 end
