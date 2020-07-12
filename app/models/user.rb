b07f53c64f9c5d08a112ca9bdb7493606e1507ca
hatori-r <aruue368@gmail.com>
2020-07-13 02:06:40 +0900

    add_follow_function_02

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,29 @@ class User < ApplicationRecord
   has_many :comments
   has_many :like, dependent: :destroy
   has_many :liked_tasks, through: :likes, source: :task
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
 
   mount_uploader :image, ImageUploader
 
   def already_liked?(task)
     self.like.exists?(task_id: task.id)
   end
+
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_141811) do
+ActiveRecord::Schema.define(version: 2020_07_12_150754) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 2020_07_02_141811) do
     t.index ["task_id"], name: "index_likes_on_task_id"
     t.index ["user_id", "task_id"], name: "index_likes_on_user_id_and_task_id", unique: true
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
   end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -58,4 +68,6 @@ ActiveRecord::Schema.define(version: 2020_07_02_141811) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "relationships", "users"
+  add_foreign_key "relationships", "users", column: "follow_id"
 end
