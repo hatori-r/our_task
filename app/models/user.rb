f102e31db676e5f6ddcee5d61046b01cd936507c
hatori-r <aruue368@gmail.com>
2020-07-15 17:43:17 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :tasks
-  has_many :comments
+  has_many :tasks, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :like, dependent: :destroy
   has_many :liked_tasks, through: :likes, source: :task
-  has_many :relationships
+  has_many :relationships, dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverse_of_relationships, source: :user
